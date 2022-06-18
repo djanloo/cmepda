e35fdb2d68f8e28c68f3a084805598324e63bb71
djanloo <zzibecu@gmail.com>
2022-06-18 18:41:57 +0200

    Doc testing

--- cloudatlas/test.py
+++ cloudatlas/test.py
@@ -9,3 +9,19 @@ def dummy(oh):
             an int lol
     """
     pass
+
+class AngerIssue:
+    """This is merely a test for documentation.
+    
+    Attributes
+    ----------
+
+        u : int
+            literally you
+    """
+    def __init__(self):
+        pass
+
+    def commit_murder(self):
+        """Again a doc test, readthedocs is pissing me off. Pls don't judge me."""
+        print("you die lol")
