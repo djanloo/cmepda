1a1d6d945c8887dcb5558e693c6a1561858b66f2
djanloo <zzibecu@gmail.com>
2022-06-18 18:20:50 +0200

    Autodoc setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import mock
 
+sys.path.insert(0, os.path.abspath('..'))
+autodoc_mock_imports = ["numpy", 'matplotlib', 'scipy']
 
 # -- Project information -----------------------------------------------------
 
-project = 'cloud atlas'
-copyright = '2022, Lushloo'
+project = 'cloudatlas'
+copyright = '2021, Lushloo'
 author = 'Lushloo'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,9 +32,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
-
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -53,3 +53,37 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'fontenc': '\\usepackage{fontenc}',
+    'fontpkg': '''\
+\\setmainfont{TeX Gyre Termes}
+\\setsansfont{TeX Gyre Termes}
+\\setmonofont{DejaVu Sans Mono}''',
+    'geometry': '\\usepackage[vmargin=2.5cm, hmargin=3cm]{geometry}',
+    'preamble': '''\
+\\usepackage[titles]{tocloft}
+\\cftsetpnumwidth {1.25cm}\\cftsetrmarg{1.5cm}
+\\setlength{\\cftchapnumwidth}{0.75cm}
+\\setlength{\\cftsecindent}{\\cftchapnumwidth}
+\\setlength{\\cftsecnumwidth}{1.25cm}''',
+    'fncychap': '\\usepackage[Bjornstrup]{fncychap}',
+    'printindex': '\\footnotesize\\raggedright\\printindex',
+     'extraclassoptions': 'openany,oneside'
+}
+latex_show_urls = 'footnote'
+
+#added because of the raise of ones * int error
+class Mock(mock.MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+    def __mul__(self, other):
+        return Mock()
+    def __rmul__(self, other):
+        return Mock()
+    def __pow__(self, other):
+        return Mock()
+    def __div__(self, other):
+        return Mock()
