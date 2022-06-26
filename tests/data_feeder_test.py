941d9820d5dc3a96069079fb8f3cda637b1a91c9
djanloo <zzibecu@gmail.com>
2022-06-26 10:47:32 +0200

    Added TestCase for bug #2

--- tests/data_feeder_test.py
+++ tests/data_feeder_test.py
@@ -2,15 +2,21 @@
 # Make posible importing modules from parent directory
 import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-import matplotlib.pyplot as plt
+import unittest
 from cloudatlas.utils import DataFeeder, animate_time_series
-from rich import print
-test_data = DataFeeder("splitted_dataset/test_splitted_data").feed()
 
-a, b, c = next(test_data)
+class dfTest(unittest.TestCase):
 
-print(f"{a.shape} , {b.shape} , {c.shape}")
+    def setUp(self):
+        self. test_data = DataFeeder("splitted_dataset/test_splitted_data").feed()
+        self.toa, self.ts, self.outcome = next(self.test_data)
 
-u = animate_time_series(b[16])
-plt.show()
+    def test_order(self):
+        # Check for bug #2
+        # Since the order is toa, ts, outcome shapes must be
+        self.assertEqual(self.toa.shape[1:], (9,9,1))
+        self.assertEqual(self.ts.shape[1:], (80,9,9))
+
+    def test_anim(self):
+        # Animation is not shown, only checks for errors
+        u = animate_time_series(self.ts[0])
