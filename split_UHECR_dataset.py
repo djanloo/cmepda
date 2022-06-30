ff866fd184bc685280e7aa63962406e4c4028423
djanloo <zzibecu@gmail.com>
2022-06-30 18:29:36 +0200

    Added funky_dtype and splitting by entry

--- split_UHECR_dataset.py
+++ split_UHECR_dataset.py
@@ -5,11 +5,10 @@ Has to be executed once.
 Reminder: original structure is (event, 9x9 detectors, 80 ts + 1 toa)
 """
 from pd4ml import Airshower
-import os
 from os.path import join
 import cloudatlas.utils as utils
 import numpy as np
-
+from rich import print
 THINNING = 1  # Thinning factor: takes one sample in every fixed number
 
 for mode in ["test", "train"]:
@@ -19,14 +18,18 @@ for mode in ["test", "train"]:
 
     # Time of arrival is stored as a 9x9 matrix (for each event)
     toa = x["features"][0][::THINNING, :, -1].reshape((-1, 9, 9, 1))
+    print(f"toa has dtype={toa.dtype}")
 
     # Time series is stored as a 80-array of 9x9 matrices (for each event)
     # A swap of axes is then required
     time_series = x["features"][0][::THINNING, :, :-1]
     time_series = np.swapaxes(time_series, 1,2).reshape((-1, 80, 9, 9))
+    print(f"time_series has dtype={time_series.dtype}")
+
 
     y = y[::THINNING]
-
+    print(f"outcome has dtype={y.dtype}")
+    exit()
     # Clean useless big file
     del x
 
---
+++ split_in_files.py
@@ -0,0 +1,43 @@
+"""Since (issue #4) keras needs a different 'style' of generator
+I decided to split each entry of the dataset in a single file.
+This will probably cause a slowdown of data generation because of reading times
+"""
+
+import numpy as np
+from cloudatlas import utils
+from os import mkdir
+from os.path import exists, join
+from rich.progress import track
+DIR = "data_by_entry"
+
+## I use the previously splitted dataset because
+# loading the full one happened once and it was a miracle
+test_feeder = utils.DataFeeder("splitted_dataset/test_splitted_data")
+train_feeder = utils.DataFeeder("splitted_dataset/train_splitted_data")
+
+# Save the datum with a specific dtype because I still don't trust indexing on dtype=object
+funky_dtype = np.dtype([    ("outcome", np.float64),
+                            ("time_series", np.float32, (80,81) ),
+                            ("toa", np.float32, (9,9,1) )
+                        ])
+
+if not exists(DIR):
+    mkdir(DIR)
+    mkdir(f"{DIR}/train")
+    mkdir(f"{DIR}/test")
+
+# Brutally use a counter because i'm lazy
+c = 0
+for data_block in track(test_feeder.feed(), total=test_feeder.n_of_parts):
+
+    block_time_series = data_block['time_series'].reshape((-1, 80, 81))
+    block_toas = data_block['toa']
+    block_outcomes = data_block['outcome']
+
+    for ts, toa, out in zip(block_time_series, block_toas, block_outcomes):
+        entry = np.empty(1, dtype=funky_dtype)
+        entry["toa"] = toa
+        entry["time_series"] = ts
+        entry["outcome"] = out 
+        np.save(f"{DIR}/test/part_{c}", entry)
+        c += 1
