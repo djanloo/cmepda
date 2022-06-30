65cd01492c6fa84710ed22a02f81851d9dee44aa
djanloo <zzibecu@gmail.com>
2022-06-30 12:55:52 +0200

    started using fit on generator ( #4 )

--- cloudatlas/lstm_encoder.py
+++ cloudatlas/lstm_encoder.py
@@ -36,13 +36,13 @@ def get_net():
 
     # Time series branch
     input_ts = Input(shape=(80,81), name="time_series" )
-    lstm = LSTM(128)(input_ts)
-    dense = Dense(128, activation='relu')(lstm)
+    lstm = LSTM(64)(input_ts)
+    dense = Dense(9, activation='relu')(lstm)
     long_short_term_memory = Model(inputs=input_ts, outputs=dense)
 
     # Concatenation
     conc = concatenate([encoder.output, long_short_term_memory.output])
-    z = Dense(128, activation="relu")(conc)
+    z = Dense(16, activation="relu")(conc)
     z = Dense(4, activation="linear")(z)
     z = Dense(1, activation="linear")(z)
     global_model = Model(inputs=[encoder.input, long_short_term_memory.input], outputs=z)
@@ -51,32 +51,30 @@ def get_net():
     # plot_model(global_model, show_shapes=True)
     return global_model
 
+def adapt_fmt(gen):
+    """Adapts the format of DataFeeder to the one required by fit() to fit a generator"""
+    for u in gen:
+        yield ([u['toa'], u['time_series'].reshape((-1, 80, 81))],
+                 u['outcome'])
+
 # Training/Loading
 def train_and_resolution(path):
     model = utils.ask_load(path)
     if model is None:
         global_model = get_net()
-        for _ in [0, 1, 3]:
-            for data_block in track(train_feeder.feed(), 
-                                    total=train_feeder.n_of_parts,
-                                    description=f"Super-epoch {_} of {path}"):
-                # Flattens features
-                time_series = data_block["time_series"].reshape((-1, 80, 81))
-
-                history = global_model.fit(
-                    x=[data_block["toa"], time_series],
-                    y=data_block["outcome"],
-                    epochs=20,
-                    batch_size=128,
-                    shuffle=True,
-                    verbose=0,
-                )
-            global_model.save(path)
-            try:
-                telegram_send.send(messages=[f"Ended super-epoch {_} for {path}",
-                                        f"Last sqr loss was {np.sqrt(history.history['loss'][-1])}"])
-            except:
-                print("Network failed")
+        
+        history = global_model.fit(
+            x=adapt_fmt(train_feeder.feed()), # fit_generator is deprecated, this can be done
+            epochs=3, # This must be so because each block has 300 events (?)
+            batch_size=128,
+            shuffle=True,
+            verbose=1,
+            )
+        global_model.save(path)
+        try:
+            telegram_send.send(messages=[f"Last sqr loss was {np.sqrt(history.history['loss'][-1])}"])
+        except:
+            print("Network failed")
     else:
         global_model = model
 
