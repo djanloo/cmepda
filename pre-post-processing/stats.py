6384eeec69141c8cb2c7c515730dec586a9c3268
djanloo <zzibecu@gmail.com>
2022-07-04 16:07:23 +0200

    Added interpercentile ranges to stats

--- pre-post-processing/stats.py
+++ pre-post-processing/stats.py
@@ -9,27 +9,76 @@ from cloudatlas.utils import DataFeeder
 from rich.progress import track
 from scipy.stats import gaussian_kde
 
-from matplotlib import rcParams
+from matplotlib import rcParams, cm
 rcParams['font.family'] = 'serif'
 
+from rich.progress import track
+FILE = "true_vs_predictions.npy"
+if not os.path.exists(FILE):
+    model = utils.ask_load("trained/accuracy_lstm_enc_1120epochs")
+    if model is None:
+        exit("Dumb")
 
-model = utils.ask_load("trained/accuracy_lstm_enc_1120epochs")
-if model is None:
-    exit("Dumb")
+    test_feeder = DataFeeder("splitted_dataset/test_splitted_data")
 
-test_feeder = DataFeeder("splitted_dataset/test_splitted_data")
+    predictions = np.array([])
+    true_vals = np.array([])
 
-predictions = np.array([])
-true_vals = np.array([])
+    for block in track(test_feeder.feed(), total=test_feeder.n_of_parts):
+        predictions = np.concatenate((predictions, model.predict([block["toa"], block["time_series"].reshape((-1, 80, 81))], verbose=0).squeeze() ))
+        true_vals = np.concatenate((true_vals, block['outcome']))
 
-for block in track(test_feeder.feed(), total=test_feeder.n_of_parts):
-    predictions = np.concatenate((predictions, model.predict([block["toa"], block["time_series"].reshape((-1, 80, 81))], verbose=0).squeeze() ))
-    true_vals = np.concatenate((true_vals, block['outcome']))
+    np.save(FILE, np.stack((true_vals, predictions)))
 
+else:
+    true_vals, predictions = np.load(FILE)
+
+
+## Quantile band
+print("Quantile band started")
+plt.figure(3)
+predictions /= true_vals
+plt.scatter(true_vals, predictions,s=0.5, alpha=0.1, color='k')
+
+N = 30
+vals = np.linspace(650, 1000, N)
+delta_quants = [80, 50, 25, 10]
+ups = np.zeros((len(delta_quants), N))
+downs = np.zeros((len(delta_quants), N))
+colormap = cm.get_cmap("plasma")
+
+# Interval percentile estimation
+for i, delta_quant in enumerate(delta_quants):
+    for segment in track(range(N-1)):
+        mask = (true_vals >= vals[segment])&(true_vals < vals[segment+1])
+        down, up = np.percentile(predictions[mask], [50 - delta_quant/2, 50 + delta_quant/2])
+        m = np.mean(predictions[mask])
+        ups[i, segment] = up
+        downs[i, segment] = down
+
+# Fill each band
+for i in range(len(delta_quants)-1):
+    plt.fill_between(vals[:-1], downs[i,:-1], downs[i+1, :-1], 
+                    color=colormap(1-delta_quants[i]/100.), alpha=0.5, label=f"{delta_quants[i]}")
+    plt.fill_between(vals[:-1], ups[i,:-1], ups[i+1, :-1], 
+                    color=colormap(1-delta_quants[i]/100.), alpha=0.5)
+plt.fill_between(vals[:-1], ups[-1,:-1], downs[-1,:-1], label=f"{delta_quants[-1]}",color=colormap(1-delta_quants[-1]/100.), alpha=0.5)
+plt.legend()
+
+plt.xlabel("true height [m]")
+plt.ylabel("Normalized predictions [a.u.]")
+plt.title("Heteroskedastic interpercentile ranges")
+
+plt.xlim(640, 1010)
+plt.ylim(0.75,1.15)
+
+plt.show()
+exit()
+## KDE 
 data = np.stack((predictions, true_vals), axis=0)
 kern = gaussian_kde(data)
 
-x = np.linspace(650, 850, 100)
+x = np.linspace(650, 850, 200)
 y = x.copy()
 
 X,Y = np.meshgrid(x,y)
@@ -58,3 +107,5 @@ plt.title("Conditional density")
 plt.xlabel("true")
 plt.ylabel("estimated")
 plt.show()
+
+
