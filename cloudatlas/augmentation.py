75cb2bfdf2c4b4c4b0c59d11fc10803221413c8c
luciapapalini <l.papalini1@studenti.unipi.it>
2022-07-08 16:26:34 +0200

    #10 Silly functions that acts on matrices

--- cloudatlas/augmentation.py
+++ cloudatlas/augmentation.py
@@ -1,6 +1,21 @@
 """Module for augmentation of data."""
-
-import os
 import numpy as np
 
-def rotation():
+def flip_matrix(matrix, mode):
+    """Flips the matrix with different axis modes.
+    Modes can be 'up-down', 'left-right', 'diagonal'."""
+    if mode == "up-down":
+        matrix = matrix[::-1]
+        return matrix
+
+    if mode == 'left-right':
+        for row in range(len(matrix)):
+            matrix[row] = matrix[row][::-1]
+            return matrix
+
+    if mode == 'diagonal':
+        matrix = np.transpose(matrix)
+        return matrix
+
+def rotate_matrix(matrix, pi_multiple=0.5):
+    """Rotate the matrix of different multiples of pi."""
