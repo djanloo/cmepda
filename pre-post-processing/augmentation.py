2a725dc4cb727f6cfcc6a8742bd9e1e3d22594b8
luciapapalini <l.papalini1@studenti.unipi.it>
2022-07-09 15:23:32 +0200

    a little bit bestia di ripetitività

--- pre-post-processing/augmentation.py
+++ pre-post-processing/augmentation.py
@@ -17,25 +17,47 @@ class Augument:
     def augment_dataset(self, dataset, tot):
         # Initialize a new record with the custom dtype
         new_record_rot = np.empty(1, dtype=funky_dtype)
-        new_record_flip = np.empty(1, dtype=funky_dtype)
+        new_record_flip_lr = np.empty(1, dtype=funky_dtype)
+        new_record_flip_ud = np.empty(1, dtype=funky_dtype)
+        new_record_flip_diag = np.empty(1, dtype=funky_dtype)
 
         for i, file in enumerate(dataset):
             # rotations
+            tot += 1
             new_record_rot['toa'] = self.rotate_matrix(dataset['toa'])
             new_record_rot['time_series'] = self.rotate_matrix(dataset['time_series'])
+            new_record_rot['outcome'] = dataset["outcome"]
+            np.save(f'part_{tot}.npy', new_record_rot)
 
-            # flips
-            new_record_flip['toa'] = self.flip_matrix(dataset['toa'], mode='left-right')
-            new_record_flip['time_series'] = self.flip_matrix(dataset['time_series'], mode='left-right')
-h
+            # flips left-right
+            tot += 1
+            new_record_flip_lr['toa'] = self.flip_matrix(dataset['toa'], mode='left-right')
+            new_record_flip_lr['time_series'] = self.flip_matrix(dataset['time_series'], mode='left-right')
+            new_record_flip_lr['outcome'] = dataset["outcome"]
+            np.save(f'part_{tot}.npy', new_record_flip_lr)
+
+            # flips up-down
+            tot += 1
+            new_record_flip_ud['toa'] = self.flip_matrix(dataset['toa'], mode='up-down')
+            new_record_flip_ud['time_series'] = self.flip_matrix(dataset['time_series'], mode='up-down')
+            new_record_flip_ud['outcome'] = dataset["outcome"]
+            np.save(f'part_{tot}.npy', new_record_flip_ud)
+
+            # flip diagonal
+            tot += 1
+            new_record_flip_diag['toa'] = self.flip_matrix(dataset['toa'], mode='diagonal')
+            new_record_flip_diag['time_series'] = self.flip_matrix(dataset['time_series'], mode='diagonal')
+            new_record_flip_diag['outcome'] = dataset["outcome"]
+            np.save(f'part_{tot}.npy', new_record_flip_diag)
+
 
     @staticmethod
     def rotate_matrix(matrix, angle=90):
         """Rotate the matrix of different angles."""
         if angle == 90:
-            for column in range(len(matrix)):
-                matrix[:, column] = matrix[:, column][::-1]
-                matrix = np.transpose(matrix)
+            matrix = np.flipud(matrix)
+            matrix = np.transpose(matrix)
+            return matrix
 
     @staticmethod
     def flip_matrix(matrix, mode):
@@ -47,7 +69,7 @@ h
 
         if mode == 'left-right':
             for row in range(len(matrix)):
-                matrix[row] =
+                matrix[row] = np.fliplr(matrix)
                 return matrix
 
         if mode == 'diagonal':
