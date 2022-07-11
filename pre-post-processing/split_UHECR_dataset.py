87460ce436a96e2e1a54d68ab609c5f28b29e8c9
luciapapalini <l.papalini1@studenti.unipi.it>
2022-07-11 14:33:09 +0200

    split dataset is back again

--- pre-post-processing/split_UHECR_dataset.py
+++ pre-post-processing/split_UHECR_dataset.py
@@ -17,6 +17,53 @@ from rich import print
 
 THINNING = 1  # Thinning factor: takes one sample in every fixed number
 
+
+def split_dataset(data, filename, axes_names, n_of_files, parent=None):
+    """Splits the dataset in smaller parts.
+    Args
+    ----
+        data : tuple
+            the data to be splitted, given in the following format:
+                (array1, array2, array3, ... )
+        filename : str
+            the root of the files' names
+        axes_names : list of str
+            the list of names for the axes
+        n_of_files : int
+            the number of parts
+        parent : str, optional
+            the folder where the splitted dataset is placed into.
+    """
+    if len(axes_names) != len(data):
+        raise ValueError(
+            f"axes names and data dimension mismatch:\n"
+            + f"len(data) = {len(data)}\tlen(axes_names) = {len(axes_names)}"
+        )
+    directory = ""
+    if parent is not None:
+        if not exists(parent):
+            os.mkdir(parent)
+        directory = parent
+    directory = f"{directory}/{filename}_splitted_data"
+    if not exists(directory):
+        os.mkdir(directory)
+        for ax in axes_names:
+            os.mkdir(join(directory, ax))
+
+    for axis, axis_data in track(
+        zip(axes_names, list(data)),
+        description=f"saving files for '{filename}'..",
+        total=n_of_files,
+    ):
+        splitted_data = np.array_split(axis_data, n_of_files)
+        for i, section in enumerate(splitted_data):
+            np.save(f"{directory}/{axis}/{filename}_part_{i}", section)
+
+    # Make config file
+    splitConf.FromNames(directory, axes_names)
+    return directory
+
+
 for mode in ["test", "train"]:
     # Load the dataset
     x, y = Airshower.load_data(mode)
@@ -38,7 +85,7 @@ for mode in ["test", "train"]:
     # Clean useless big file
     del x
 
-    test_directory = utils.split_dataset(
+    test_directory = split_dataset(
         (toa, time_series, y),
         mode,
         ["toa", "time_series", "outcome"],
