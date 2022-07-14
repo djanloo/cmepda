836145ea737da8896b38c39438016d102c54e713
djanloo <zzibecu@gmail.com>
2022-07-14 10:53:53 +0200

    closed tests for #15

--- tests/test_issue_15.py
+++ tests/test_issue_15.py
@@ -1,45 +1,56 @@
 import os
-from tensorflow import keras
-from keras.utils.vis_utils import plot_model
-
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from keras.utils.vis_utils import plot_model
+import matplotlib.pyplot as plt
+from rich import print
 
 from context import FeederProf, DataFeeder
 from context import LstmEncoder, ToaEncoder, TimeSeriesLSTM
-
-from matplotlib import pyplot as plt
 
 # constants
 EPOCHS = 50
 BATCH_SIZE = 128
 
-# options
+# Encoder subnet
+
+# options for feeders
+# Note that the input field is different for the subnets
 feeder_options = {
     "shuffle": True,
     "batch_size": BATCH_SIZE,
-    "input_fields": "time_series",
+    "input_fields": "toa",
     "target_field": "outcome",
 }
 
 train_feeder = DataFeeder("data_by_entry/train", **feeder_options)
 val_feeder = DataFeeder("data_by_entry/validation", **feeder_options)
-test_feeder = DataFeeder("data_by_entry/test", **feeder_options)
 
 # initializing TimeSeries class
-lstm = TimeSeriesLSTM()
+enc = ToaEncoder(earlystopping=True, tensorboard=True)
 
-
-# TensorBoard callbacks, # Write TensorBoard logs to `./logs` directory
-tb_callbacks = keras.callbacks.TensorBoard(
-    log_dir=f"{lstm.path}/logs", histogram_freq=1
+enc.train(
+    x=train_feeder,
+    epochs=EPOCHS,
+    validation_data=val_feeder,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    use_multiprocessing=False,
 )
+
+# LSTM subnet
+feeder_options["input_fields"] = "time_series"
+
+train_feeder = DataFeeder("data_by_entry/train", **feeder_options)
+val_feeder = DataFeeder("data_by_entry/validation", **feeder_options)
+
+lstm = TimeSeriesLSTM(earlystopping=True, tensorboard=True)
 
 lstm.train(
     x=train_feeder,
     epochs=EPOCHS,
     validation_data=val_feeder,
     batch_size=BATCH_SIZE,
-    callbacks=[tb_callbacks],
     verbose=1,
     use_multiprocessing=False,
 )
