c117ab5364200c3f0574f0f1eedf8817c0681a2d
djanloo <zzibecu@gmail.com>
2022-07-19 11:14:27 +0200

    Added MISO test for DataFeeders

--- tests/general/test_datafeeders.py
+++ tests/general/test_datafeeders.py
@@ -11,7 +11,11 @@ DUMMY_SHAPE = (3,4)
 N = 100
 
 DATASET_DTYPE_SISO = [("input", np.float32, DUMMY_SHAPE),
-                 ("output", np.float32)]
+                    ("output", np.float32)]
+
+DATASET_DTYPE_MISO = [  ("input1", np.float32, DUMMY_SHAPE),
+                        ("input2", np.float32, DUMMY_SHAPE),
+                        ("output", np.float32)]
 
 FEEDER_OPTS_SISO =  {
     "shuffle": True,
@@ -19,9 +23,15 @@ FEEDER_OPTS_SISO =  {
     "input_fields": "input",
     "target_field": "output",
 }
+FEEDER_OPTS_MISO = {
+    "shuffle": True,
+    "batch_size": 10,
+    "input_fields": ["input1", "input2"],
+    "target_field": "output",
+}
 
 FOLDER_SISO = "dataset_folder_siso"
-
+FOLDER_MISO = "dataset_folder_miso"
 
 class TestSISO(unittest.TestCase):
     """Single input single output test"""
@@ -42,4 +52,27 @@ class TestSISO(unittest.TestCase):
 
     def test_zclearup(self):
         rmtree(FOLDER_SISO)
+
+
+
+class TestMISO(unittest.TestCase):
+    """Single input single output test"""
+    def setUp(self):
+        dataset = np.empty(N, dtype=DATASET_DTYPE_MISO)
+        dataset["input1"] = np.random.uniform(0, 1, size=(N,) + DUMMY_SHAPE)
+        dataset["input2"] = np.random.uniform(0, 1, size=(N,) + DUMMY_SHAPE)
+        dataset["output"] = np.random.uniform(-1, 0, size=(N,))
+        
+        # Splits the dataset in files
+        if not os.path.exists(FOLDER_MISO):
+            os.mkdir(FOLDER_MISO)
+        
+        for i, record in enumerate(dataset):
+            np.save(f"{FOLDER_MISO}/{constants.FILENAME.format(name=i)}", record)
+
+    def test_feeder(self):
+        jimbo = DataFeeder(FOLDER_MISO, **FEEDER_OPTS_MISO)
+
+    def test_zclearup(self):
+        rmtree(FOLDER_MISO)
         
