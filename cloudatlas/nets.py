0d045138708bcd5c4a5173fe351cc852d92a20b3
djanloo <zzibecu@gmail.com>
2022-07-25 11:20:49 +0200

    STUPID BUG #17

--- cloudatlas/nets.py
+++ cloudatlas/nets.py
@@ -150,7 +150,7 @@ class ToaEncoder(LushlooNet):
 
         # Now adds an output layer
         # This will be removed when used in LstmEncoder
-        enc = Dense(1, activation="linear", name="enc_out")(flat)
+        enc = Dense(1, activation="linear", name="enc_out")(enc)
 
         self.model = Model(inputs=input_toa, outputs=enc, name="ToAEncoder")
         self.model.compile(**self.compilation_kwargs)
